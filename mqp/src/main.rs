852491bb95e4df57961dfc3535de84fbd7d30fea
wmh <_@wmh.us.com>
2022-02-18 21:08:41 -0500

    Generate the CLI skeleton

--- mqp/src/main.rs
+++ mqp/src/main.rs
@@ -1,7 +1,31 @@
 // This file is dual licensed under the terms of the Apache License, Version
 // 2.0, and the BSD License. See the LICENSE file in the root of this repository
 // for complete details.
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Cli {
+    #[clap(global = true, short, long)]
+    target: Option<String>,
+
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Install {},
+    Uninstall {},
+    Upgrade {},
+}
 
 fn main() {
-    mqp::hello();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Install {} => {}
+        Commands::Uninstall {} => {}
+        Commands::Upgrade {} => {}
+    }
 }
