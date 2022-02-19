b5752d7e616af2bb3642eec4379214db4bac7345
wmh <_@wmh.us.com>
2022-02-19 14:15:00 -0500

    Start parsing the repositories.yml file

--- libmqp/src/lib.rs
+++ libmqp/src/lib.rs
@@ -2,25 +2,51 @@
 // 2.0, and the BSD License. See the LICENSE file in the root of this repository
 // for complete details.
 
+use std::fs::File;
 use std::path::{Path, PathBuf};
+
+use serde::Deserialize;
 use thiserror::Error;
+use url::Url;
 
 const REPOSITORY_FILENAME: &str = "repositories.yml";
 
 #[derive(Error, Debug)]
 pub enum MQPackageError {
+    #[error("no repository configuration file")]
+    NoRepositoryConfig {
+        #[from]
+        source: std::io::Error,
+    },
+
+    #[error("invalid repository configuration")]
+    InvalidRepositoryConfig {
+        #[from]
+        source: serde_yaml::Error,
+    },
+
     #[error("unable to locate a valid directory")]
     NoTargetDirectoryFound,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Repository {
+    pub repositories: Vec<Url>,
 }
 
 #[derive(Debug)]
 pub struct Config {
     target: PathBuf,
+    repositories: Repository,
 }
 
 impl Config {
     pub fn target(&self) -> &Path {
         self.target.as_path()
+    }
+
+    pub fn repositories(&self) -> &Repository {
+        &self.repositories
     }
 }
 
@@ -29,8 +55,14 @@ impl Config {
     where
         P: Into<PathBuf>,
     {
+        let path = path.into();
+        let repofile = File::open(path.join(REPOSITORY_FILENAME))
+            .map_err(|source| MQPackageError::NoRepositoryConfig { source })?;
+        let repos: Repository = serde_yaml::from_reader(repofile)?;
+
         Ok(Config {
-            target: path.into(),
+            target: path,
+            repositories: repos,
         })
     }
 
