91bfe049f35f538bca2a7b8861d3fa0d8e95c69f
wmh <_@wmh.us.com>
2022-02-26 13:15:59 -0500

    Remove temporary debugging code

--- mqpkg/src/types.rs
+++ mqpkg/src/types.rs
@@ -98,14 +98,6 @@ impl Candidate {
         &self.version
     }
 
-    pub(crate) fn repository(&self) -> Option<&Repository> {
-        self.repository.as_ref().map(|r| &r.1)
-    }
-
-    pub(crate) fn repository_id(&self) -> Option<isize> {
-        self.repository.as_ref().map(|r| r.0)
-    }
-
     pub(crate) fn dependencies(&self) -> &HashMap<PackageName, VersionReq> {
         // This code will panic if someone calls dependencies without this Candidate
         // having dependencies. This is important, both Candidate::new() and
